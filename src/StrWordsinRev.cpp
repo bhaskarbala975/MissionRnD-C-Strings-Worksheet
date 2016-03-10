66a6a06bd831c2a5abaf29296f41db1e4411ef02
bhaskarbala975 <bhaskarbala975@missionrnd.com>
2016-03-10 13:28:26 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -14,6 +14,41 @@ NOTES: Don't create new string.
 
 void str_words_in_rev(char *input, int len)
 {
-	input=strrev(input);
-	
+	int i = 0, temp = 0, j = 0, k = 0, max, temp1, max1;
+	char t;
+	input = strrev(input);
+label: i = temp;
+	for (; i < len; i++)
+	{
+		if (input[i] == ' ')
+		{
+			break;
+		}
+	}
+	max = i - 1;
+	max1 = temp;
+	while (max1 < max)
+	{
+		t = input[max];
+		input[max] = input[max1];
+		input[max1] = t;
+		max1++;
+		max--;
+	}
+	temp1 = i+1;
+	for (k = temp1; k < len; k++)
+	{
+		if (input[k] == ' ')
+		{
+
+		}
+		else
+			break;
+	}
+
+	temp = k;
+	if (input[temp] != '\0')
+		goto label;
+
+
 }
